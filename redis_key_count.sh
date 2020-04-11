d5e11d88777d14f4963e03575d89a914bb9d63e0
Sagar <sagarr8130@gmail.com>
2020-04-11 17:00:15 +0530

    Redis Key Count Expect Shell Script

---
+++ redis_key_count.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/expect
+
+###############################################################################
+#Script Name    : redis.sh
+#Description    : Key Count of multiple Redis-WFM servers
+#Author         : Sagar Saxena
+#Email          : sagar.saxena@kronos.com
+################################################################################
+
+set prompt "(%|#|\\$) $"
+set HOST [lindex $argv 0]
+set PASS [lindex $argv 1]
+set timeout 90
+set f [open "IP_HostNames"]
+set values [split [read -nonewline $f]]
+close $f
+foreach i $values {
+spawn ssh -q $HOST@$i
+expect "yes/no" {
+        send "yes\r"
+        expect ": " { send "$PASS\r" }
+        } ": " {send "$PASS\r" }
+expect -re  $prompt
+                 send "sudo docker exec redis redis-cli info keyspace|grep -i db0 | cut -d, -f1\r"
+                 expect ": " {
+                 send "$PASS\r"
+                 } ": " { send "$PASS\r" }
+expect -re $prompt
+                send "exit\r"
+interact
+}
